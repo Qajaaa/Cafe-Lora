4cffc9a437d75c9e58673783d0533b18163a70b0
Alena Holubova <aja.holubova@email.cz>
2024-06-30 13:18:17 +0200

    vytvoreni komponenty layer

--- src/components/Drink/drink.jsx
+++ src/components/Drink/drink.jsx
@@ -1,21 +1,28 @@
 import './drink.css';
+import { Layer } from '../Layer/layer';
 
-export const Drink = ({ name, image }) => {
-    return (
-      <div className="drink">
-        <div className="drink__product">
-          <div className="drink__cup">
-            <img src={image} alt={name} />
-          </div>
-          <div className="drink__info">
-            <h3>{name}</h3>
-            <div className="drink__controls">
-              <button className="order-btn">
-                Objednat
-              </button>
-            </div>
+export const Drink = ({ name, image, layers }) => {
+  return (
+    <div className="drink">
+      <div className="drink__product">
+        <div className="drink__cup">
+          <img src={image} alt={name} />
+        </div>
+        <div className="drink__info">
+          <h3>{name}</h3>
+          <div className="layers">
+            {layers && layers.map((layer, index) => (
+              <Layer key={index} color={layer.color} label={layer.label} />
+            ))}
           </div>
         </div>
       </div>
-    );
-  };
+      <form className="drink__controls">
+        <input type="hidden" className="order-id" value="0" />
+        <button className="order-btn">
+          Objednat
+        </button>
+      </form>
+    </div>
+  );
+};
